30abd51b00635b400605bbd27aea6fe605f04368
Andrew Liu <liuyiqiandrew@gmail.com>
2024-08-07 17:13:19 -0400

    TF est from ee, need testing

---
+++ cmb_diagnoistics/tf_calib.py
@@ -0,0 +1,196 @@
+from .utils import *
+import healpy as hp
+import pymaster as nmt
+import numpy as np
+import itertools
+import pygsm
+import scipy.optimize as opt
+import matplotlib.pyplot as plt
+
+class TF_Calib:
+    def __init__(self, nside=512):
+
+        self.nside = nside
+
+        # initialize planck
+        self.planck_freqs = np.array([100, 143, 217, 353])
+        self.planck_eff_freqs = np.array([101.31, 142.71, 221.915, 361.29])
+        self.planck_dust_eff_freqs = np.array([105.25, 148.235, 229.097, 372.193])
+        self.planck_beams = np.array([9.66, 7.27, 5.01, 4.86])
+
+        self.planck_fname = '/home/yl9946/projects/tp_leakage/planck_equatorial/planck_{}_equatorial_rm_mnp_dp.fits'
+        self.planck_f2 = {}
+        
+        # initialize so
+        self.so_fname = "/scratch/gpfs/sa5705/shared/SO_SAT/satp3_maps/8May2024/map_f{freq:03d}_muKcmb.fits"
+        self.so_freqs = np.array([90, 150])
+        self.so_beams = np.array([27.4, 17.6])
+        self.so_f2 = {}
+        
+        # mask
+        self.mask = None
+        self.fsky = None
+
+        # namaster power spectra parameters
+        self.bins = None
+        self.bin_width = None
+        self.e_l = None
+        self.e_dl2cl = None
+        self.msk = None
+
+        # camb reference values
+        self.camb_dl_path = "/scratch/gpfs/yl9946/basic_science/camb_lens_nobb.dat"
+        self.camb_dl = None
+
+        # planck EE
+        self.ee_specs = {}
+        self.ee_xspec_var = {}
+
+        self.so_auto = {}
+        self.so_x_planck = {}
+        self.so_x_planck_var = {}
+
+        self.tf = None
+
+        self.result_dir = "/home/yl9946/projects/cmb_diagnostics/result/"
+
+
+    def init_mask(self, mask):
+        print("init mask")
+        self.mask = mask
+        self.fsky = self.mask.sum() / self.mask.shape[0]
+
+    def init_mask_from_box(self, box):
+        print("init mask")
+        mask = box2hpmask(self.nside, box)
+        self.mask = apodize_square_mask(mask)
+        self.fsky = self.mask.sum() / self.mask.shape[0]
+
+    def init_nmt(self, bin_width=20, lmin=30, lmax=300):
+        print("init NaMaster")
+        self.bins = nmt.NmtBin.from_nside_linear(self.nside, bin_width, is_Dell=True)
+        self.bin_width = bin_width
+        self.e_l = self.bins.get_effective_ells()
+        self.e_dl2cl = 2 * np.pi / self.e_l / (self.e_l + 1)
+        self.msk = (self.e_l < lmax) * (self.e_l > lmin)
+
+    def init_camb_dl(self):
+        print("read CAMB dl")
+        dl_tmp = np.loadtxt(self.camb_dl_path)
+        dl_tmp = np.concatenate((np.zeros((1, 5)), dl_tmp))
+        dl_bin = self.bins.bin_cell(dl_tmp[:self.nside * 3, 2])
+        self.camb_dl = dl_bin
+
+    def init_planck_f2(self):
+        print("init planck spin 2 fields")
+        for f, fwhm in zip(self.planck_freqs, self.planck_beams):
+            print(f)
+            gbeam = hp.gauss_beam(fwhm / 60 / 180 * np.pi, self.nside * 3 - 1)
+            plk_qu = hp.read_map(self.planck_fname.format(f), field=[1, 2]) * 1e6
+            plk_qu = hp.ud_grade(plk_qu, 512)
+            self.planck_f2[f] = nmt.NmtField(self.mask, plk_qu, beam=gbeam, spin=2)
+
+    def calc_planck_specs(self):
+        print("calculating planck spectra")
+        for fq1, fq2 in itertools.combinations_with_replacement(self.planck_freqs, 2):
+            print(f"p{fq1}xp{fq2}")
+            nmt_spec = nmt.compute_full_master(self.planck_f2[fq1], self.planck_f2[fq2], self.bins)
+            self.ee_specs[f"p{fq1}xp{fq2}"] = nmt_spec[0] * 1e12
+
+    def calc_planck_xspec_var(self):
+        print("calculating planck cross spectra variance")
+        for fq1, fq2 in itertools.combinations(self.planck_freqs, 2):
+            print(f"p{fq1}xp{fq2}")
+            cl13 = self.ee_specs[f"p{fq1}xp{fq1}"]
+            cl24 = self.ee_specs[f"p{fq2}xp{fq2}"]
+            cl14 = cl23 = self.ee_specs[f"p{fq1}xp{fq2}"]
+            self.ee_xspec_var[f"p{fq1}xp{fq2}"] = knox_covar(cl13, cl24, cl14, cl23, self.fsky, self.e_l, self.bin_width)
+
+    def init_so_f2(self):
+        print("init SO spin 2 fields")
+        for fq, fwhm in zip(self.so_freqs, self.so_beams):
+            print(fq)
+            gbeam = hp.gauss_beam(fwhm / 60 / 180 * np.pi, self.nside * 3 - 1)
+            qu_map = read_carr2healpix(self.so_fname.format(freq=fq))[1:]
+            self.so_f2[fq] = nmt.NmtField(self.mask, qu_map, beam=gbeam)
+
+    def calc_so_auto(self):
+        print("calculate SO auto spectra")
+        for f in self.so_freqs:
+            print(f)
+            self.so_auto[f] = nmt.compute_full_master(self.so_f2[f], self.so_f2[f], self.bins)[0]
+
+    def calc_so_x_planck(self):
+        print("calculating planck x SO spectra")
+        plt.figure(dpi=300)
+        for fs, fp in itertools.product(self.so_freqs, self.planck_freqs):
+            print(f"s{fs}xp{fp}")
+            self.so_x_planck[f"s{fs}xp{fp}"] = nmt.compute_full_master(self.so_f2[fs], self.planck_f2[fp], self.bins)[0]
+            plt.loglog(self.e_l[self.msk], self.so_x_planck[f"s{fs}xp{fp}"][self.msk], label=f"s{fs}xp{fp}")
+        plt.legend()
+        plt.savefig(self.result_dir + "SOxPlk_spec.png")
+
+    def calc_so_x_planck_var(self):
+        print("calculating SO x planck variance")
+        for fs, fp in itertools.product(self.so_freqs, self.planck_freqs):
+            print(f"s{fs}xp{fp}")
+            cl13 = self.so_auto[fs]
+            cl24 = self.ee_specs[f"p{fp}xp{fp}"]
+            cl14 = cl23 = self.so_x_planck[f"s{fs}xp{fp}"]
+            self.so_x_planck_var[f"s{fs}xp{fp}"] = knox_covar(cl13, cl24, cl14, cl23, self.fsky, self.e_l, self.bin_width)
+
+    def estimate_tf(self):
+        print("Estimate transfer function")
+        tf_res = np.zeros((self.msk.sum(), 7))
+        for bin_ind in range(self.msk.sum()):
+            freq_data_holder_full = np.zeros((14, 6))
+            for i, ((f1, f2), (fd1, fd2)) in enumerate(zip(itertools.combinations(self.planck_freqs, 2), \
+                                                           itertools.combinations(self.planck_dust_eff_freqs, 2))):
+                freq_data_holder_full[i] = f1, f2, fd1, fd2, self.ee_specs[f"p{f1}xp{f2}"][self.msk][bin_ind], \
+                    self.ee_xspec_var[f"p{f1}xp{f2}"][self.msk][bin_ind]
+            
+            for i, (fs, (fp, fdp)) in enumerate(itertools.product(self.so_freqs, zip(self.planck_freqs, self.planck_dust_eff_freqs))):
+                freq_data_holder_full[i + 6] = fs, fp, fs, fdp, self.so_x_planck[f"s{fs}xp{fp}"][self.msk][bin_ind], \
+                    self.so_x_planck_var[f"s{fs}xp{fp}"][self.msk][bin_ind]
+
+            dust_unity = dust_dl(1., freq_data_holder_full[:, 2], freq_data_holder_full[:, 3], 1.53) \
+                * pygsm.trj2tcmb(freq_data_holder_full[:, 2]) * pygsm.trj2tcmb(freq_data_holder_full[:, 2])
+            ee = self.camb_dl[self.msk][bin_ind]
+
+            res = opt.minimize(dust_neg_lnlike, 1, (dust_unity[:6], ee, freq_data_holder_full[:6, 4], \
+                                                    freq_data_holder_full[:6, 5]))
+            a = res.x[0]
+            a_var = hess_inv(dust_unity[:6], freq_data_holder_full[:6, 5])
+            # print(res)
+            
+            res90 = opt.minimize(tf_neg_lnlike, 0.5, (a, dust_unity[6:10], ee, freq_data_holder_full[6:10, 4], \
+                                                      freq_data_holder_full[6:10, 5]))
+            tf90= res90.x[0]**2
+            dmdq = a * dust_unity[6:10] + ee
+            tmp_var = hess_inv(dmdq, freq_data_holder_full[6:10, 5])
+            tf90_var = 4 * res90.x[0]**2 * tmp_var
+            
+            res150 = opt.minimize(tf_neg_lnlike, 0.5, (a, dust_unity[10:], ee, freq_data_holder_full[10:, 4], \
+                                                       freq_data_holder_full[10:, 5]))
+            tf150= res150.x[0]**2
+            dmdq = a * dust_unity[10:] + ee
+            tmp_var = hess_inv(dmdq, freq_data_holder_full[10:, 5])
+            tf150_var = 4 * res150.x[0]**2 * tmp_var
+            
+            tf_res[bin_ind] = self.e_l[self.msk][bin_ind], a, a_var, tf90, tf90_var, tf150, tf150_var
+        self.tf = tf_res.T
+
+    def plot_and_save_result(self):
+        plt.figure(dpi=300)
+        plt.errorbar(self.tf[0], self.tf[3], self.tf[4]**0.5, ls='--', marker='.', label="90")
+        plt.errorbar(self.tf[0] + 2, self.tf[5], self.tf[6]**0.5, ls='--', marker='.', label="150")
+        plt.ylabel('transfer func')
+        plt.ylim(1e-3, 1)
+        # plt.ylim(-0.1, 1.1)
+        plt.fill_between((30, 80), (-0.1, -0.1), (1.1, 1.1), color='gray', alpha=.3)
+        plt.fill_between((180, 240), (-0.1, -0.1), (1.1, 1.1), color='gray', alpha=.3)
+        plt.legend()
+        plt.loglog()
+        plt.savefig(self.result_dir + "transfer_function.png")
+        selected_cols = [0, 3, 4, 5, 6]
+        np.save(self.result_dir + "tf_ee.npy", self.tf[selected_cols].T)
---
+++ cmb_diagnoistics/utils.py
@@ -0,0 +1,66 @@
+import numpy as np
+import healpy as hp
+from pixell import enmap, reproject
+import pymaster as nmt
+import pygsm
+
+def box2hpmask(nside, box):
+    """
+    Convert carr box to square mask
+    """
+    pix_ind = np.arange(hp.nside2npix(nside))
+    pix_ang = np.array(hp.pix2ang(nside, pix_ind, lonlat=True)) / 180 * np.pi
+    if box[1,1] < 0:
+        ra_cut = (pix_ang[0] > (2 * np.pi + box[0, 1])) * (pix_ang[0] < (2 * np.pi + box[1, 1]))
+    else:
+        ra_cut = (pix_ang[0] > box[0, 1]) * (pix_ang[0] < box[1, 1])
+    dec_cut = (pix_ang[1] > box[0, 0]) * (pix_ang[1] < box[1, 0])
+    return ra_cut * dec_cut
+
+
+def apodize_square_mask(mask):
+    ZERO = 1e-4
+    nhg = hp.smoothing(mask, 4 / 180 * np.pi)
+    nhg[nhg < 0] = 0
+    nhg /= nhg.max()
+    tmp_mask = nhg > ZERO
+    ap_mask = nmt.mask_apodization(tmp_mask, 10, "C2")
+    return ap_mask
+
+
+def read_carr2healpix(carr_path):
+    carr_map = enmap.read_fits(carr_path)
+    hpx_map = reproject.map2healpix(carr_map, method='spline', order=1)
+    return hpx_map
+
+
+def knox_covar(cl13, cl24, cl14, cl23, fsky, e_l, bin_wth):
+    """
+    Calculate covariance according to Knox Formula
+    """
+    return (cl13 * cl24 + cl14 * cl23) / (2 * e_l + 1) / fsky / bin_wth
+
+
+def log_gauss_like(data, model, var):
+    return (- (data - model)**2 / var).sum()
+
+
+def dust_dl(a, f1, f2, beta):
+    mbb1 = f1**beta * pygsm.planck_law(19.6, f1)
+    mbb2 = f2**beta * pygsm.planck_law(19.6, f2)
+    mbb_bench = 353.**beta * pygsm.planck_law(19.6, 353.)
+    return a * mbb1 * mbb2 / (mbb_bench)**2
+
+
+def dust_neg_lnlike(a, dust_unity, ee, data, var):
+    model = dust_unity * a + ee
+    return -log_gauss_like(data, model, var)
+
+
+def tf_neg_lnlike(f, a, dust_unity, ee, data, var):
+    model = f * (a * dust_unity + ee)
+    return -log_gauss_like(data, model, var)
+
+
+def hess_inv(dmdq, var):
+    return 1 / (dmdq**2 / var).sum()
