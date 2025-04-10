02a334fea92320149a3adaaf636af6f142b2d097
Andrew Liu <liuyiqiandrew@gmail.com>
2025-04-10 12:29:45 -0400

    update container test

--- test/new_container_test.py
+++ test/new_container_test.py
@@ -3,6 +3,7 @@ import healpy as hp
 import cmb_diagnostics
 import pymaster as nmt
 from cmb_diagnostics.cmb_diagnoistics.Constants import PSType
+from cmb_diagnostics.cmb_diagnoistics.diag_utils import read_carr2healpix
 import cmb_utils
 import itertools
 import matplotlib.pyplot as plt
@@ -25,7 +26,7 @@ def main():
     planck_fnames = [f'/home/yl9946/projects/tp_leakage/planck_equatorial/planck_{f}_equatorial_rm_mnp_dp.fits' for f in planck_freqs]
     
     plk_field_container = cmb_diagnostics.Container.NmtFieldContainer("Planck")
-    print("Create Fields")
+    print("Create Planck Fields")
     for idx, fname in enumerate(planck_fnames):
         pmaps_raw = hp.read_map(fname, field=[0, 1, 2]) * 1e6
         pmaps = hp.ud_grade(pmaps_raw, 512)
@@ -34,14 +35,38 @@ def main():
         plk_field_container.add_spin0_field(planck_freqs[idx], f0)
         plk_field_container.add_spin2_field(planck_freqs[idx], f2)
 
+    so_beam_fwhm = np.array([27.4, 17.6])
+    so_beams = [hp.gauss_beam((fwhm / 60 / 180) * np.pi, 512 * 3) for fwhm in so_beam_fwhm]
+    so_freqs = np.array([90, 150])
+    so_fnames = [f"/scratch/gpfs/sa5705/shared/SO_SAT/satp3_maps/cmb_maps_satp3_20240714/map_f{freq:03d}_muKcmb.fits" for freq in so_freqs]
+
+    so_field_container = cmb_diagnostics.Container.NmtFieldContainer("SO")
+    print("Create SO Field")
+    for idx, fname in enumerate(so_fnames):
+        so_map = read_carr2healpix(fname)
+        f0 = nmt.NmtField(mask, so_map[:1], beam=so_beams[idx])
+        f2 = nmt.NmtField(mask, so_map[1:], beam=so_beams[idx])
+        so_field_container.add_spin0_field(so_freqs[idx], f0)
+        so_field_container.add_spin2_field(so_freqs[idx], f2)
+        
+
     bins = nmt.NmtBin.from_nside_linear(512, 20, True)
 
-    print("Calculate power spectrum")
+    print("Calculate planck auto power spectrum")
     plk_pp_calculator = cmb_diagnostics.PSCalculator(
         plk_field_container, plk_field_container, bins, fsky, [PSType.PP]
     )
     plk_pp_calculator.calculate_spectra()
+
+    print("Calculate so x planck auto power spectrum")
+    sp_pp_calculator = cmb_diagnostics.PSCalculator(
+        so_field_container, plk_field_container, bins, fsky, [PSType.PP]
+    )
+    sp_pp_calculator.calculate_spectra()
+
     plk_pp = plk_pp_calculator.pp_container
+    sp_pp = sp_pp_calculator.pp_container
+
     e_l = plk_pp_calculator.eff_ell
     msk = (e_l > 30) * (e_l < 300)
 
@@ -51,7 +76,14 @@ def main():
         dl = plk_pp.get_spectrum('EE', f1, f2)
         plt.loglog(e_l[msk], dl[msk], label=f"{f1}x{f2}")
     plt.legend()
-    plt.savefig('test.png')
+    plt.savefig('test_planck_auto.png')
+    
+    plt.figure()
+    for f1, f2 in itertools.product(so_freqs, planck_freqs):
+        dl = sp_pp.get_spectrum('EE', f1, f2)
+        plt.loglog(e_l[msk], dl[msk], label=f"{f1}x{f2}")
+    plt.legend()
+    plt.savefig("test_sxp.png")
 
 
 
